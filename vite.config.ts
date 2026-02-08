68e7bfcb423ac6ba8bd8fa50c4cbfbe9b6105cb9
Aishah <maishah2540@gmail.com>
2026-02-08 18:54:24 +0200

    deployy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/queen-ng-s-easy-invoice/" : "/",
+  base: "/queen-ng-s-easy-invoice/",
   server: {
     host: "::",
     port: 8080,
